5a44d7277cacf22b308bcc8fef9190d970a0a272
TanyaChas <tetiana2045@gmail.com>
2023-11-14 17:30:06 +0200

    HomeWork_1

---
+++ HomeWork_1.java
@@ -0,0 +1,12 @@
+public class HomeWork_1 {
+    class myClass {
+        int Year = 2023;
+        public static int MYMETHOD(int a,
+                                   int b){
+            int c = a + b; //sum of 2 numbers
+            return c;}
+        public static void main(String[]
+                                        args) {
+            MYMETHOD(1, 2);}
+    }
+}
