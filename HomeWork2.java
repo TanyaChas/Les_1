8d4dd50745e7932f3a275df5c1293498af1c2ed2
TanyaChas <tetiana2045@gmail.com>
2023-11-16 21:01:38 +0200

    HomeWork2

---
+++ HomeWork2.java
@@ -0,0 +1,15 @@
+public class HomeWork2 {
+        public static void main(String[] args) {
+            byte a = 120;
+            byte b = -10;
+
+            int x = a + 50;
+
+
+            System.out.println(a + b);
+            System.out.println(x);
+            System.out.println(a > b);
+            System.out.println(a < b);
+
+        }
+}
