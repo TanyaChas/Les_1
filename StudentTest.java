f4675de5f83e730cd007f88230e3147a8b4be00d
TanyaChas <tetiana2045@gmail.com>
2023-11-24 20:30:58 +0200

    HW4

---
+++ StudentTest.java
@@ -0,0 +1,15 @@
+public class StudentTest {
+
+    private String student1;
+    private String student2;
+    private String student3;
+    StudentTest student1 = new student1("10236", "Alex","Miller", 5, "mathematics = 5", "economic = 5", "english = 4");
+
+//            2 = new student("11102", "Rey","Grey", 1, "mathematics = 5", "economic = 5", "english = 5");
+//            3 = new student("10236", "Luna","Moon", 4, "mathematics = 3", "economic = 3", "english = 4");
+    );
+}
+// Попрошу вас розписати це завдання, бо шось воно не пішло. На лекціях ми розбираєсо одного студента,
+// а в завданні 3, тож мені складно виконати завдання, щось я роблю не так і не зрозуміла що.
+// Як новачок в цьому щось дуже складний курс. Чи можемо ми розбирати завдання на мітенгу,
+// замість питань теорії, краще б розібрали одне з завдань.
